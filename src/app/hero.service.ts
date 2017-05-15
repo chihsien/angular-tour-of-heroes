db93e76382b5e95abd7183fdbd2de32556dd30fa
Gary Chen <gary_chen@migocorp.com>
2017-05-15 10:54:09 +0800

    'update'

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+}
+
+}
+
+
